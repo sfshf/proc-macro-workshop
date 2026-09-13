f7fb1d66117aec597bcfb139bcdf826839b5fea0
sfshf <sfshf@github.com>
2026-09-13 22:11:09 +0800

    tmp

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,103 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{
+    parse_macro_input, spanned::Spanned, Data, DeriveInput, GenericArgument, Ident, PathArguments,
+    Type::Path,
+};
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = &input.ident;
+    let Data::Struct(data) = &input.data else {
+        return syn::Error::new_spanned(name, "Builder can only be derived for structs")
+            .to_compile_error()
+            .into();
+    };
+    let mut methods: Vec<proc_macro2::TokenStream> = Vec::new();
+    for field in &data.fields {
+        for attr in &field.attrs {
+            if attr.path().is_ident("builder") {
+                _ = attr.parse_nested_meta(|meta| {
+                    if !meta.path.is_ident("each") {
+                        panic!("expected `builder(each = \"...\")`");
+                    };
+                    let method_name = meta.value()?.parse::<syn::LitStr>()?.value();
+                    let method_name = Ident::new(&method_name, method_name.span());
+                    let field_name = field.ident.as_ref().unwrap();
+                    let field_type = match &field.ty {
+                        Path(path) => path,
+                        _ => panic!("expected a path type for the field"),
+                    };
+                    let segment = field_type.path.segments.last().unwrap();
+                    if segment.ident != "Vec" {
+                        panic!("expected a Vec type for the field");
+                    };
+                    let PathArguments::AngleBracketed(args) = &segment.arguments else {
+                        panic!("expected angle bracketed arguments for the Vec type");
+                    };
+                    let GenericArgument::Type(ty) = args.args.first().unwrap() else {
+                        panic!("expected a type argument for the Vec type");
+                    };
+                    methods.push(quote! {
+                        pub fn #method_name(&mut self, arg: #ty) -> &mut Self {
+                            if let Some(ref mut #field_name) = self.#field_name {
+                                #field_name.push(arg);
+                            } else {
+                                self.#field_name = Some(vec![arg]);
+                            }
+                            self
+                        }
+                    });
+                    Ok(())
+                });
+            };
+        }
+    }
+    let expanded = quote! {
+        pub struct CommandBuilder {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+        impl #name {
+            pub fn builder() -> CommandBuilder {
+                CommandBuilder {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+        impl CommandBuilder {
+            #(#methods)*
+            pub fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+            pub fn envs(&mut self, envs: Vec<String>) -> &mut Self {
+                self.env = Some(envs);
+                self
+            }
+            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }
+            pub fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
+                Ok(#name {
+                    executable: self.executable.clone().ok_or("executable is not set")?,
+                    args: self.args.clone().unwrap_or_default(),
+                    env: self.env.clone().unwrap_or_default(),
+                    current_dir: self.current_dir.clone().unwrap_or_default(),
+                })
+            }
+        }
+    };
+    expanded.into()
 }
--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,61 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput, Expr, LitStr, Meta};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = &input.ident;
+    let field_name = ident.to_string();
+    let mut generics = input.generics.clone();
+    for param in &mut generics.params {
+        if let syn::GenericParam::Type(type_param) = param {
+            type_param.bounds.push(syn::parse_quote!(std::fmt::Debug));
+        }
+    }
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    let Data::Struct(data) = &input.data else {
+        panic!("CustomDebug can only be derived for structs");
+    };
+    let mut fields: Vec<proc_macro2::TokenStream> = Vec::new();
+    for field in &data.fields {
+        let field_name = &field.ident;
+        let field_name_str = field_name.as_ref().unwrap().to_string();
+        let mut field_format: Option<&LitStr> = None;
+        for attr in &field.attrs {
+            let path = attr.path();
+            if path.is_ident("debug") {
+                let Meta::NameValue(name_value) = &attr.meta else {
+                    panic!("debug attribute must be a name-value pair");
+                };
+                let Expr::Lit(syn::ExprLit {
+                    lit: syn::Lit::Str(lit_str),
+                    ..
+                }) = &name_value.value
+                else {
+                    panic!("debug attribute value must be a string literal");
+                };
+                field_format = Some(lit_str);
+            }
+        }
+        if let Some(format_str) = field_format {
+            fields.push(quote! {
+                .field(#field_name_str, &format_args!(#format_str, &self.#field_name))
+            });
+        } else {
+            fields.push(quote! {
+                .field(#field_name_str, &self.#field_name)
+            });
+        }
+    }
+    let expanded = quote! {
+        impl #impl_generics std::fmt::Debug for #ident #ty_generics #where_clause {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                f.debug_struct(#field_name)
+                #(#fields)*
+                .finish()
+            }
+        }
+    };
+    TokenStream::from(expanded)
 }
